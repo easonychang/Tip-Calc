e45e3d178ff8b7f6c940a4ba2470a56b5ae82cb2
easonychang <easonchangar1997@gmail.com>
2017-08-21 20:40:07 -0700

    build successfully, database/header integrated

---
+++ Tip Calc/Tip_Cal_Bridging_Header.h
@@ -0,0 +1,18 @@
+//
+//  Tip_Cal_Bridging_Header.h
+//  Tip Calc
+//
+//  Created by Eason Chang on 8/21/17.
+//
+//
+
+#ifndef Tip_Cal_Bridging_Header_h
+#define Tip_Cal_Bridging_Header_h
+
+
+#endif /* Tip_Cal_Bridging_Header_h */
+
+#import "FMDatabase.h"
+#import "FMDatabaseAdditions.h"
+#import "FMDatabaseQueue.h"
+#import "FMResultSet.h"
